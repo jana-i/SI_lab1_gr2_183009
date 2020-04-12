868baacb3d2f6314d7854b5508cb741945e0620d
jana-i <63495041+jana-i@users.noreply.github.com>
2020-04-12 23:36:38 +0200

    Add files via upload

---
+++ Faculty.java
@@ -0,0 +1,10 @@
+class Faculty{
+	List<String> Student = new ArrayList<String>();
+	String nameCollege;
+	//metod za brishenje na studenti
+	public void deleteStudent(){
+		
+	}
+	
+}
+
---
+++ Student.java
@@ -0,0 +1,34 @@
+class Student {
+	String index;
+	String firstName;
+	String lastName;
+ArrayList<Integer> grades =new ArrayList<Integer>();
+	//TODO constructor
+
+	//TODO seters & getters
+public Integer getIndex(){
+                return index;
+        }
+        public String getFirstName(){
+                return firstName;
+        }
+        public String getLastName(){
+                return lastName;
+        }
+public void setIndex(Integer newIndex){
+                this.index = newIndex;
+        }
+        public void setFirstName(String newFirstName){
+                this.firstName = newFirstName;
+        }
+        public void setLastName(String newLastName){
+                this.lastName = newLastName;
+        }
+	public double getAverage() {
+		//TODO
+	}
+
+	public int ECTSCredits() {
+		//TODO
+	}
+}
